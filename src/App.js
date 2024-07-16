fdea58087b7df5597d1274862ee9ddc95d27323d
vknanavati <vimin.nanavati@gmail.com>
2024-07-16 13:45:41 -0400

    stored input data into local storage, retrieved data from local storage for display in input box.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,48 @@ import { useState } from 'react';
 
 function App() {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState(() => {
+    return localStorage.getItem("first name")
+  });
+  const [lastName, setLastName] = useState(() => {
+    return localStorage.getItem("last name")
+  });
+  const [age, setAge] = useState(() => {
+    return localStorage.getItem("age")
+  });
+  const [email, setEmail] = useState(() => {
+    return localStorage.getItem("email")
+  });
 
   console.log("first name:", firstName)
   console.log("last name:", lastName)
   console.log("age:", age)
   console.log("email:", email)
 
+
+
+  localStorage.setItem("first name", firstName);
+  localStorage.setItem("last name", lastName);
+  localStorage.setItem("age", age)
+  localStorage.setItem("email", email)
+
   return (
     <>
+      <h2>Information</h2>
       <form>
         <div>
           <label>First Name: </label>
           <input
+            type="text"
+            value={firstName}
             onChange={e => setFirstName(e.target.value)} />
         </div>
 
         <div>
           <label>Last Name: </label>
           <input
+            value={lastName}
+            type="text"
             onChange={e => setLastName(e.target.value)}
           />
         </div>
@@ -31,6 +51,8 @@ function App() {
         <div>
           <label>Age: </label>
           <input
+            value={age}
+            type="text"
             onChange={e => setAge(e.target.value)}
           />
         </div>
@@ -38,6 +60,8 @@ function App() {
         <div>
           <label>Email Address: </label>
           <input
+            value={email}
+            type="email"
             onChange={e => setEmail(e.target.value)}
           />
         </div>
