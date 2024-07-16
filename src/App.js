61ce93e0e876ed3558a6262cfc9dc1744b7178f0
vknanavati <vimin.nanavati@gmail.com>
2024-07-16 09:50:36 -0400

    set up labels and input fields.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log("first name:", firstName)
+  console.log("last name:", lastName)
+  console.log("age:", age)
+  console.log("email:", email)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <div>
+          <label>First Name: </label>
+          <input
+            onChange={e => setFirstName(e.target.value)} />
+        </div>
+
+        <div>
+          <label>Last Name: </label>
+          <input
+            onChange={e => setLastName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Age: </label>
+          <input
+            onChange={e => setAge(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Email Address: </label>
+          <input
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
